255f2e1b8b4ff5e9456f7d5cb6adf2498c70bef8
Truong Thi Huong Giang <giangtth@vnext.vn>
2023-10-26 15:31:35 +0700

    most replayed

---
+++ main.js
@@ -0,0 +1,49 @@
+let localStream;
+let remoteStream;
+let peerConnection;
+
+const servers = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    },
+  ],
+};
+
+let init = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false,
+  });
+  document.getElementById('user-1').srcObject = localStream;
+};
+
+let createOffer = async () => {
+  peerConnection = new RTCPeerConnection(servers);
+
+  remoteStream = new MediaStream();
+  document.getElementById('user-2').srcObject = remoteStream;
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  peerConnection.ontrack = (event) => {
+    event.streams[0].getTracks().forEach((track) => {
+      remoteStream.addTrack();
+    });
+  };
+
+  peerConnection.onicecandidate = async (event) => {
+    if (event.candidate) {
+      console.log('New ICE candidate: ', event.candidate);
+    }
+  };
+
+  let offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+
+  console.log('Offer', offer);
+};
+
+init();
